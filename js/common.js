c4439f994b22b384532517eaf52afe78bf794df5
jonio-uhei <73581065+jonio-uhei@users.noreply.github.com>
2021-02-19 15:33:36 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -4,70 +4,28 @@ $(function(){
   $('.burger-btn').on('click',function(){
     $('.burger-btn').toggleClass('close');
     $('.nav-wrapper').fadeToggle(500);
+    $('.nav__fade').addClass('nav__fade');
+    $('.nav__fade1').addClass('nav__fade1');
+    $('.nav__fade2').addClass('nav__fade2');
+    $('.nav__fade3').addClass('nav__fade3');
   });
   // ここまでハンバーガーメニュー
 
 
-  //ここからスムーススクロール
-  $('a[href^="#"]').click(function() {
+  //ここからアコーディオン
+    $('.question-response__list').click(function() {
+      var $answer = $(this).find('.question-response__answer');
+      if($answer.hasClass('open')) { 
+        $answer.removeClass('open');
+        $answer.slideUp();
+        $(this).find('span').text('+');
+      } else {
+        $answer.addClass('open'); 
+        $answer.slideDown();
+        $('span').text('-');
+        $(this).find('span').text('-');
+      }
+    });
+  //ここまでアコーディオン
 
-    let speed = 500;
-    let href= $(this).attr("href");
-    let target = $(href == "#" || href == "" ? 'html' : href);
-    let position = target.offset().top;
-
-    $('.burger-btn').click();
-    $('body,html').animate({scrollTop:position}, speed, 'swing');
-    
-    return false;
-  });
-  //ここまでスムーススクロール
-  
-
- 
-
-
-  $('#login-show').click(function() {
-    $('#login-modal').fadeIn();
-  });
-
-  $('.signup-show').click(function() {
-    $('#signup-modal').fadeIn();
-  });
-
-  $('.close-modal').click(function() {
-    $('#login-modal').fadeOut();
-    $('#signup-modal').fadeOut();
-  });
-  
-  $('.lesson-hover').hover(
-    function() {
-      $(this).find('.text-contents').addClass('text-active'); 
-    },
-    function() {
-      $(this).find('.text-contents').removeClass('text-active');
-    }
-  );
-
-  // FAQのアコーディオン
-  $('.question-response__list').click(function() {
-    var $answer = $(this).find('.question-response__answer');
-    if($answer.hasClass('open')) { 
-      $answer.removeClass('open');
-      // slideUpメソッドを用いて、$answerを隠してください
-      $answer.slideUp();
-
-      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
-      $(this).find('span').text('+');
-      
-    } else {
-      $answer.addClass('open'); 
-      // slideDownメソッドを用いて、$answerを表示してください
-      $answer.slideDown();
-      
-      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
-      $('span').text('-');
-      $(this).find('span').text('-');
-    }
-  });
 });
